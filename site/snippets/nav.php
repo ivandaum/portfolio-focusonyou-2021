3430ccb452ed9eb381b78d5f6e60652ae05c3eb9
Ivan Daum <ivan.daum@gmail.com>
2021-03-17 17:55:48 +0100

    style(nav): add responsive

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,16 +1,30 @@
 <?php
   $projects = $site->index()->filterBy('template', 'projects')->children();
   $categories = array();
+  $columns = array();
 
   foreach($projects as $project) {
     $cat = $project->category()->toString();
-    
     if (!isset($categories[$cat])) {
       $categories[$cat] = array();
     }
 
     $categories[$cat][] = $project;
   }
+
+  $i = 0;
+  foreach($categories as $name => $projects) {
+    if(!isset($columns[$i])) $columns[$i] = array();
+
+    $columns[$i][] = array(
+      'title' => $name,
+      'projects' => $projects
+    );
+
+    $i++;
+    if($i >= 3) $i = 0;
+  }
+  
 ?>
 <div class="Nav container has-width-100 is-flex is-justified-x is-fixed">
     <a href="<?= $site->url() ?>">Logo</a>
@@ -19,22 +33,28 @@
       <p class="is-center-y is-flex">Fermer <span class="is-block Nav__iconClose is-flex is-wrap is-center-y is-relative"></span></p>
     </button>
 </div>
-<div class="Nav__content has-width-100 is-fixed has-height-100 is-flex is-center">
-  <div class="container has-width-100 is-flex is-center-y">
-    <ul class="is-column has-mr-1 is-4 Nav__menu">
+<div class="Nav__content has-width-100 is-fixed has-height-100 is-flex is-center" hidden>
+  <div class="Nav__scroller container has-width-100 is-flex is-center-y is-wrap">
+    <ul class="Nav__menu is-column has-mr-1 is-4 is-12-phone">
     <?php foreach($site->pages()->listed() as $page): ?>
       <li><a href="<?= $page->url() ?>"><?= $page->title() ?></a></li>
     <?php endforeach; ?>
     </ul>
-    <div class="Nav__categories is-column is-7 is-flex is-wrap is-right-x">
-    <?php foreach($categories as $name => $category): ?>
-      <ul class="is-column is-4">
-        <li class="has-color-grey"><?= $name ?></li>
-        <?php foreach($category as $project): ?>
-        <li class="has-fontsize-20"><a href="<?= $project->url() ?>"><?= $project->title() ?></a></li>
-        <?php endforeach; ?>
-      </ul>
-    <?php endforeach; ?>
+    <div class="Nav__categories is-column is-7 is-12-phone is-flex is-wrap is-right-x">
+
+    <?php foreach($columns as $column): ?>
+      <div class="is-column is-4 is-6-touch">
+      <?php foreach($column as $category): ?>
+        <ul>
+          <li class="has-color-grey"><?= $category['title'] ?></li>
+          <?php foreach($category['projects'] as $project): ?>
+          <li class="has-fontsize-20"><a href="<?= $project->url() ?>"><?= $project->title() ?></a></li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endforeach; ?>
+      </div>
+      <?php endforeach; ?>
+
     </div>
   </div>
 </div>
