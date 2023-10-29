d067b8e6581d58d132da3582c29bd78fa6da6323
Ivan Daum <ivan.daum@gmail.com>
2023-10-29 13:25:25 +0100

    fix lot of errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,14 @@
 const path = require('path')
-const { VueLoaderPlugin } = require('vue-loader')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './assets/index.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index.js',
   },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
-    },
-  },
   module: {
     rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader',
-      },
       {
         test: /\.(png|jpg|jpeg|webp)$/,
         use: 'url-loader',
@@ -28,34 +17,22 @@ module.exports = {
         test: /\.js$/,
         use: 'babel-loader',
       },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '',
-            },
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: 'url-loader',
       },
       {
-        test: /\.(vert|frag)$/i,
-        use: ['raw-loader', 'glslify-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
     ],
   },
-  plugins: [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'index.css',
-    }),
-  ],
 }
